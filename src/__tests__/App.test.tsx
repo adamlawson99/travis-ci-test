2da81a5d0eda168d6086129966d81e2117e7cc38
adam lawson <alaws057@uottawa.ca>
2021-02-13 09:35:53 -0500

    add travis CI support

---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { render, fireEvent, waitForElement } from "@testing-library/react";
+
+import App from "../../App";
+
+describe("<App />", () => {
+  test("should display a blank screen with some text", async () => {});
+});
